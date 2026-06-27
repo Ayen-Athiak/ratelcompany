16208a52dc69b00cd4e98edc6ce3b3a433b63b4d
Ayen-Guech <a.guech@alustudent.com>
2026-06-27 19:46:40 +0200

    Initial commit - Ratel General Trading website

--- src/pages/FAQ.tsx
+++ src/pages/FAQ.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { motion } from 'framer-motion'
 import { ChevronDown, ArrowRight, HelpCircle } from 'lucide-react'
 import { Link } from 'react-router-dom'
 
